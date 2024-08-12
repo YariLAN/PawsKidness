8ab7cf1e05802ccd4f6efe5e54683194cdd50d44
YariLAN <LordYarik22@mail.ru>
2024-08-12 22:32:09 +0300

    Add Volunteer Entity

---
+++ PawsKindness.Backend/src/PawsKindness.Domain/Models/Volunteer.cs
@@ -0,0 +1,39 @@
+﻿namespace PawsKindness.Domain.Models;
+
+public class Volunteer
+{
+    public Guid Id { get; set; }
+
+    public string Surname { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string MiddleName { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public int YearsExperience { get; set; }
+
+    public int NumberPetsFoundAHome { get; set; }
+
+    public int NumberPetsLookingForAHome { get; set; }
+
+    public int NumberPersNeedHelp { get; set; }
+
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public List<SocialNetwork> SocialNetworks { get; set; } = [];
+
+    public List<Requisite> Requisites { get; set; } = [];
+
+    public List<Pet> Pets { get; set; } = []; 
+}
+
+public class SocialNetwork
+{
+    public Guid Id { get; set; }
+
+    public string Url { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+}
